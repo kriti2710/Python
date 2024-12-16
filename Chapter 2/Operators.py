e536d6b50d26832cf216d70bdc2a0fd82d2775ae
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-12-16 16:51:09 +0530

    Add files via upload

---
+++ Chapter 2/Operators.py
@@ -0,0 +1,23 @@
+# a = 7
+# b = 4
+# c = a+b
+# print(c)
+
+#assignment operator
+# a = 4-2
+# b = 6
+
+# list = (1, 2,3,4,5)
+# t = type(list)
+# print(t)
+
+# a = int(input("enter number 1: "))
+# b = int(input("enter number 2: "))
+# print("a is greater than b is:", a>b)
+
+
+a = int(input("enter number 1: "))
+print("sqaure: ", a**2)
+
+
+
---
+++ Chapter 2/input.py
@@ -0,0 +1,9 @@
+# a = input("enetr a value of a: ")
+# b = input("enter a value of b: ")
+
+# print("sum is: " ,a+b)
+
+a = int(input("enetr a value of a: "))
+b = int(input("enter a value of b: "))
+
+print("sum is: " ,a+b)
